90bfae855d7cebdb8ce51d70a23c9e633e65293a
Jugantar Madhukalya <53754834+JugantarM@users.noreply.github.com>
2021-12-08 18:19:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter UserName");
  var b=prompt("Enter password");
-  	if(a== "Priyanka" && b=="20-12-2002")
+  	if(a== "Priyanka" && b=="29-12-2002")
   	{
   	    window.location.href="start.html";
   	}
